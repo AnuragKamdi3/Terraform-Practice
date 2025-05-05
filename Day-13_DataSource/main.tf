0310539b815407d1cac346bb991a28ad7d1b201e
AnuragKamdi3 <anuraggkamdi@gmail.com>
2025-05-06 01:27:39 +0530

    Day-13

--- Day-13_DataSource/main.tf
+++ Day-13_DataSource/main.tf
@@ -4,9 +4,28 @@ data "aws_subnet" "name" {
     values = ["Def_SN-1"] # insert value here
     }
 }
-
+data "aws_ami" "amzlinux" {
+    most_recent = true
+    owners = [ "amazon" ]
+    filter {
+        name = "name"
+        values = [ "aws-elasticbeanstalk-amzn-2023.3.20231211.64bit-eb_python39_amazon_linux_2023-hvm-2023-12-16T22-51" ]
+    }
+    filter {
+        name = "root-device-type"
+        values = [ "ebs" ]
+    }
+    filter {
+        name = "virtualization-type"
+        values = [ "hvm" ]
+    }
+    filter {
+        name = "architecture"
+        values = [ "x86_64" ]
+    }
+}
 resource "aws_instance" "name" {
-    ami ="ami-002f6e91abff6eb96"
+    ami =data.aws_ami.amzlinux.id
     instance_type = "t2.micro"
     subnet_id = data.aws_subnet.name.id
 }
